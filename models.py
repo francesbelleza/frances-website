d3e3872ebe0d00d441e59bf2ff666d159839a040
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-04-05 01:11:53 -0700

    setup backend & database for blog

---
+++ models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=db.func.current_timestamp())
